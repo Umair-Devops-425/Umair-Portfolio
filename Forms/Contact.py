ab9c8111b284c754eb065133902a88eed9dfb0c6
Umair <umairmohd425@gmail.com>
2024-12-05 16:17:21 +0530

    pushing filees

---
+++ Forms/Contact.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def contact_form():
+    with st.form("contact_form"):
+        name = st.text_input("Full name")
+        email = st.text_input("Email Address")
+        message = st.text_area("Your Message")
+        submit_button = st.form_submit_button("Submit")
+
+        if submit_button:
+            st.success("Message Succesfully Sent!")
+
+        
--- Resume.py
+++ Resume.py
@@ -1,3 +1,29 @@
 import streamlit as st
 
-st.title("Hello")
+about_page = st.Page(
+    page="Views/aboutme.py",
+    title="About Me",
+    # icon=":material/account_circles:",
+    default=True,
+)
+project_1_page = st.Page(
+    page="Views/chatboat.py",
+    title="Chatbot",
+    # icon=":material/smart_toys:",
+)
+
+# --Navigation Bar--
+
+pg = st.navigation(
+    {
+        "Info":[about_page],
+        "Projects":[project_1_page]
+    }
+)
+
+# --Navigation Run--
+
+pg.run()
+
+# st.logo("")
+st.sidebar.text("Made by Me")
